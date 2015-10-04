54a28df986deaee6006263b586f8a70affd85318
Randyr902 <rdj7890@gmail.com>
2015-10-04 13:27:15 -0700

    wiki controller and updated views

---
+++ app/controllers/wiki_controller.rb
@@ -0,0 +1,42 @@
+class WikiController < ApplicationController
+
+  def index
+    @wikis = Wiki.all
+  end
+
+  def show
+    @wikis = Wiki.find(params[:id])
+  end
+
+  def create
+    @wiki = Wiki.new (params.require(:wiki).permit(:title, :body, :private))
+    if @wiki.save
+      flash[:notice] = "Wiki was saved!"
+      redirect_to @wiki
+    else
+      flash[:error] = "Wiki couldn't be saved. Please try again."
+      render :new
+    end
+  end 
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def wiki_params
+    params.require(:wiki).permit(:title, :body, :private)
+end
+
+  def find_wiki
+    @wiki = wiki.find(params[:id])
+  end
+end
