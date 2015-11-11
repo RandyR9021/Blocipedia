4a79b7b25fbe561ff2900efcbdadfcf0dd5b858c
Randyr902 <rdj7890@gmail.com>
2015-11-11 11:31:29 -0800

    added haml and css to landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resources :users, only: [:show]
+
   resources :wikis
 
-  root to:  'wikis#index'
+  authenticated :user do
+    root to:  'wikis#index', as: :authenticated_root
+  end
+
+
+  root to: 'welcome#index'
+
 end
